6039fc92aafb9a491e8d1cfd009ef454ebf96f56
Marika <liteolika@gmail.com>
2025-01-20 16:58:03 +0100

    new commit with more functionality

--- script.js
+++ script.js
@@ -23,67 +23,85 @@
 /*
     Tiny utility functions
 */
-
 // querySelector
 const select = (selector, parent = document) => parent.querySelector(selector);
-
 // querySelectorAll, but returns selected elements in an array
 const selectAll = (selector, parent = document) => [...parent.querySelectorAll(selector)];
-
-// Save- and get stuff from local storage
-const getFromStorage = string => JSON.parse(localStorage.getItem(string));
-const saveToStorage = (string, item) => localStorage.setItem(string, JSON.stringify(item));
-
 // Returns an object with form data on submit
 const collectFormData = formElem => Object.fromEntries(new FormData(formElem));
-
-// Creates and returns an element. Takes the html-tag as a string ('div') and an object with attributes { attr-name: attr-value }
-const createElem = (tagString, attributesObj) => Object.assign(document.createElement(tagString), attributesObj);
-// reference: https://oozou.com/til/how-to-create-an-html-element-with-attributes-in-one-line-in-javascript-69
-
-
-
+/*
+    local storage functions
+*/
+const getFromStorage = key => (localStorage.getItem(key)) ? JSON.parse(localStorage.getItem(key)) : false;
+const saveToStorage = (key, item) => localStorage.setItem(key, JSON.stringify(item));
 /*
     Data / Collections
 */
-
 // DOM Elements for later use
 const bucketFormElem = select('#bucketForm');
 const bucketListsElem = select('#bucketLists');
 
-
 // Categories (All available categories are listed here)
-let activityCategories = ['Resor', 'Äventyr', 'Lärande', 'Hobby'];
-// (change this to a set maybe?)
+const activityCategories = new Set(['Resor', 'Äventyr', 'Lärande', 'Hobby']);
 
 // Bucket List (The place to collect all saved activities)
-let bucketList = [
-    // Sample bucket list items (to be removed)
-    { activityName: 'Activity 1', activityCategory: 'Resor' },
-    { activityName: 'Activity 2', activityCategory: 'Äventyr' },
-    { activityName: 'Activity 3', activityCategory: 'Resor' },
-    { activityName: 'Activity 4', activityCategory: 'Hobby' }
-];
+const bucketList = getFromStorage('bucketList') || [];
 
+// This is used to number the activities in the order they are created, to give them a short unique identifier
+let numActivitiesCreated = getFromStorage('numActivitiesCreated') || 0;
 
 /*
    init on page load
 */
 
-// (Just something simple to start with, I will create this later)
 renderBucketList(activityCategories, bucketList);
 
 
 /*
-    Form submit event, when the user added a new activity: 
-    - Collect the form data in a new object: { activityName: value, activityCategory: value }
-    - Add a new activity from the form data...
+    Form submit event (the user added a new activity) 
+    - Collect the form data in a new activity object: { activityName: value, activityCategory: value }
+    - Add a new activity with the activity object as an argument
 */
 bucketFormElem.addEventListener('submit', event => {
     event.preventDefault();
-    const activityObj = collectFormData(event.target);
-    addNewActivity(activityObj);
+    addNewActivity(collectFormData(event.target));
 });
+
+/*
+    Click events in Bucket Lists
+    - Mark activity as completed (true/false)
+    - Delete activity item
+*/
+bucketListsElem.addEventListener("click", event => {
+    if (event.target.type === 'checkbox') {
+        const isChecked = event.target.checked;
+        updateItemInBucketList(event, (item) => {
+            item.completed = isChecked;
+        })
+    }
+    if (event.target.closest('button')) {
+        removeTargetActivity(event);
+        updateItemInBucketList(event, (item, index) => {
+            bucketList.splice(index, 1);
+        })
+    }
+});
+
+
+function updateItemInBucketList(event, callback) {
+    const elem = event.target.closest('[data-activity-number]');
+    const num = Number(elem.getAttribute('data-activity-number'));
+    bucketList.forEach((obj, index) => {
+        if (num === obj.activityNumber) {
+            callback(obj, index);
+        }
+    });
+}
+
+function removeTargetActivity(event) {
+    event.target.closest('[data-activity-number]').remove();
+}
+
 
 
 /*
@@ -92,156 +110,162 @@ bucketFormElem.addEventListener('submit', event => {
    - render the activity to the document
    - add the activity to the Bucket List array
 */
-function addNewActivity(activityObj) {
+
+function addNewActivity(obj) {
+    // The new activity object needs a unique identifier
+    ++numActivitiesCreated;
+    obj.activityNumber = numActivitiesCreated;
+
     // create activity element and add to the category list in the document
-    const newActivityElem = createActivityElem(activityObj.activityName);
-    bucketListsElem.prepend(newActivityElem);
+    const newActivityElem = getActivityHTML(obj);
+    select(`[data-category=${obj.activityCategory}]`).prepend(newActivityElem);
+    // bucketListsElem.prepend(newActivityElem);
+
     // add activity to the bucket list
-    bucketList.push(activityObj);
-    console.log('new activity', activityObj);
-
+    bucketList.push(obj);
 }
 
-
-/*
-    FUNCTIONS - Creating and adding Elements to the DOM
-    (use DocumentFragments? templates or template literals instead?)
-*/
-
-/*
-    Function: Create activity element
-    - Takes the activityName (string),
-    -  Creates and returns one activity element (with child elements)
-*/
-function createActivityElem(activityName) {
-
-    // Create a unique ID for the checkbox. Not really a 100% unique ID, but close enough for this assignment, I guess... 
-    const uniqueCheckboxID = `id-${Math.round(Date.now() * Math.random())}`;
-
-    // Elements and attributes
-    const elem = {
-        activityContainer: {
-            tag: 'li',
-            attr: {
-                classList: 'activity',
-            }
-        },
-        activityName: {
-            tag: 'p',
-            attr: {
-                textContent: activityName
-            }
-        },
-        checkbox: {
-            tag: 'input',
-            attr: {
-                type: 'checkbox',
-                id: uniqueCheckboxID
-            }
-        },
-        label: {
-            tag: 'label',
-            attr: {
-                innerText: 'Har gjort',
-                for: uniqueCheckboxID
-            }
-        },
-        // Create and return the element
-        create(elem) {
-            return createElem(this[elem].tag, this[elem].attr);
-        }
-    }
-
-    const parent = elem.create('activityContainer');
-    parent.appendChild(elem.create('activityName'));
-    parent.appendChild(elem.create('checkbox'));
-    parent.appendChild(elem.create('label'));
-
-    return parent;
-}
-
-/*
-    Function: Create a category element
-    - Takes the category name (string) and an array of activity objects to include in the category list
-    - Creates and returns one category element (with child elements/activites)
-*/
-
-function createCategoryElem(category, activities) {
-    
-    // Elements and attributes 
-    const elem = {
-        categoryContainer: {
-            tag: 'section',
-            attr: {
-                id: category,
-                classList: category
-            }
-        },
-        heading: {
-            tag: 'h2',
-            attr: {
-                textContent: category
-            }
-        },
-        list: {
-            tag: 'ul',
-            attr: {}
-        },
-        create(elem) { 
-            // Create and return the element
-            return createElem(this[elem].tag, this[elem].attr);
-        }
-    }
-
-    // Create the Activity Container Parent and add the heading
-    const parent = elem.create('categoryContainer');
-    parent.appendChild(elem.create('heading'));
-
-    // Create the activity list
-    const activityList = elem.create('list');
-
-    // Create all activities and add them to the list
-    if (activities.length > 0) {
-        activities.forEach(activity => {
-            activityList.appendChild(createActivityElem(activity.activityName));
+function renderBucketList(setOfCategories, arrayOfActivities) {
+    setOfCategories.forEach(category => {
+        const activitiesForThisCategory = arrayOfActivities.filter(obj => {
+            obj.activityCategory === category;
         });
-    }
-    parent.appendChild(activityList); 
-
-    // Return the category container element with the list of activities
-    return parent; 
-}
-
-
-
-// Just some basic functionality to start with..
-function renderBucketList(categories, activityList) {
-    /*
-    - One section element for each category
-        - A heading with the category-name
-        - A list with all the activity-items belonging to that category
-    */
-    
-    const categoryLists = [];
-
-    
-    // Create a container for each category with activities
-    categories.forEach(category => {
-
-        // const categoryList = activityList.filter()
-        const categoryElem = createCategoryElem(category, activityList)
+        const categoryElem = getCategoryHTML(category, activitiesForThisCategory);
         bucketListsElem.appendChild(categoryElem);
     });
-    
 }
 
-// ///////////// Some more stuff to implement
+// Generates and returns the html for one category section
+function getCategoryHTML(category, activitiesArray) {
+    const activities = getActivitiesHTML(activitiesArray),
+        elements = createHTMLcollection();
 
+    return elements
+        .addParent('section', { classList: `${category.toLowerCase()}` })
+        .addChild('h2', { textContent: category })
+        .addChild('ul',{ dataAttr: [ 'category', category ]})
+        .setParent('ul')
+        .addChildrenFromHTML(activities)
+        .returnHTML();
+}
+
+// Generates and returns the html for all activity objects in the array (in a document fragment)
+function getActivitiesHTML(activitiesArray) {
+    const elements = createHTMLcollection();
+    for (let activityObj in activitiesArray) {
+        elements.addChildrenFromHTML(getActivityHTML(activityObj));
+    }
+    return elements.returnHTML();
+}
+
+// Generates and returns the HTMl for one single activity (in a document fragment)
+function getActivityHTML(activityObj) {
+    const number = activityObj.activityNumber,
+        name = activityObj.activityName,
+        elements = createHTMLcollection();
+    // console.log(activityObj.activityName, activityObj)
+
+    return elements
+        .addParent('li', {
+            dataAttr: ['activity-number', number]
+        })
+        .addChild('p', {
+            textContent: name
+        })
+        .addChild('input', {
+            type: 'checkbox',
+            id: `completed-${number}`
+        })
+        .addChild('label', {
+            classList: 'visually-hidden',
+            textContent: 'har gjort aktiviteten',
+            htmlFor: `completed-${number}`
+        })
+        .addChild('button', {
+            classList: 'visually-hidden',
+        })
+        .setParent('button')
+        .addChild('span', {
+            classList: 'visually-hidden',
+            textContent: 'radera aktiviteten',
+        })
+        .returnHTML();
+}
+
+// A function that makes it less painful to generate html with `document.createElement` 
+function createHTMLcollection() {
+    const HTMLcollection_proto = {
+
+        createElem(tagStr, attrObj) {
+            let elem = Object.assign(document.createElement(tagStr), (attrObj || {}));
+            if (attrObj && attrObj.hasOwnProperty('dataAttr')) {
+                const data = attrObj.dataAttr;
+                elem.setAttribute(`data-${data[0]}`, data[1]);
+            }
+            return elem;
+        },
+        selectElem(selector, parent = this.fragment || false) {
+            if (!parent) console.error('missing parent', this);
+            return parent.querySelector(selector);
+        },
+        addParent(tagStr, attrObj) {
+            this.currentParent = this.createElem(tagStr, attrObj);
+            this.fragment.appendChild(this.currentParent);
+            return this;
+        },
+        addChild(tagStr, attrObj) {
+            if (!this.currentParent) this.currentParent = this.fragment;
+            this.currentParent.appendChild(this.createElem(tagStr, attrObj));
+            return this;
+        },
+        addChildrenFromHTML(html) {
+            if (!this.currentParent) this.currentParent = this.fragment;
+            this.currentParent.appendChild(html);
+            return this;
+        },
+        setParent(selector) {
+            const newParent = this.selectElem(selector);
+            this.currentParent = newParent;
+            return this;
+        },
+        renderHTMLTo(elem) {
+            elem.appendChild(this.fragment);
+            return this;
+        },
+        returnHTML() {
+            return this.fragment;
+        }
+    }
+    // Create a new obj with "HTMLcollection" as its prototype and with its own document fragment
+    let HTMLcollectionChild = Object.create(HTMLcollection_proto);
+    HTMLcollectionChild.fragment = document.createDocumentFragment();
+    return HTMLcollectionChild;
+}
+
+
+// ///////////// Some more stuff to implement
 /*
     Sort activities alphabetically within each category.
 */
 
-
+// Sorting and filtering functions
+function sortAlphabetically(arrOfObjects, key, lang = 'sv') {
+    return arrOfObjects.sort((obj1, obj2) => obj2[key].localeCompare(obj1[key], lang));
+}
 /*
     Save data: Use localStorage to save and load the list automatically.
 */
+
+function test(num) {
+
+    const arr = Array.from(activityCategories);
+    const cat = () => arr[Math.floor(Math.random() * arr.length)]; 
+
+    while (num > 0) {
+        addNewActivity({ activityName: 'Aktivitet', activityCategory: cat() });
+        num--;
+    }
+    numActivitiesCreated;
+}
+
+test(6)
